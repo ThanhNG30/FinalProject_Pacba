8abbc3fe9fc15e36ea0a6caae58026812fceb8e4
nnbtran2507 <ngtran2507@gmail.com>
2026-04-27 01:11:21 -0400

    Update abc.py

--- abc.py
+++ abc.py
@@ -4,8 +4,6 @@ import sys      # for exit()
 import pygame
 import math
 
-
-
 # Configure serial communication.
 #Set for Create 2 baud rate.
 #ser = serial.Serial(baudrate=115200, port="COMx")  # Windows: COM port #
@@ -20,28 +18,30 @@ else:
 ser.write(bytearray([128, 132]))  # full mode (since pick up Roomba to end)
 time.sleep(1)
 # 
-drive_straight = ser.write(bytearray([137, 0, 0, 0, 0]))
-turn_left = ser.write(bytearray([137, 0 , 185, 0, 1]))#ccw
-turn_right = ser.write(bytearray([137, 0, 185, 255, 255]))#cw
-stop = ser.write(bytearray([137, 0, 0, 0, 0]))
+drive_straight = bytearray([137, 0, 0, 0, 0])
+turn_left = bytearray([137, 0 , 185, 0, 1])#ccw
+turn_right = bytearray([137, 0, 185, 255, 255])#cw
+stop = bytearray([137, 0, 0, 0, 0])
 left_k_pressed = False
 right_k_pressed = False
 
 # Keep a counter to help control timing
 time = 0
-spinning_time = ??
+spinning_time = 0
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:            
             if event.key == pygame.K_LEFT:
                 left_k_pressed = True
-                spinning_time = 
+                spinning_time = time +30          
             elif event.key == pygame.K_RIGHT:
                 right_k_pressed = True
-                spinning_time = 
-    if time ?? spinning_time
-        #stop
-    if time ?? spinning_time
+                spinning_time = time +30
+            elif event.key == pygame.K_UP:
+                ser.write(drive_straight)
+    if time == spinning_time:
+        ser.write(stop)
+    if time < spinning_time:
         #spinning
     time += 1
 ser.close()
