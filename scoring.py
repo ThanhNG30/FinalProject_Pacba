f81447851a621bc88cfba8c51e03c9634025c5cf
Ryan Fox <ryan.fox75@qc.quincycollege.edu>
2026-05-01 18:23:16 -0400

    updates to scoring

--- scoring.py
+++ scoring.py
@@ -9,15 +9,36 @@ history of where docks have been detected to ensure they aren't scored twice.
 import ir_sensors
 import position
 
+#Consants
+DOCK_DETECTION_RANGE = 500 #In mm, actual tested range is a little below this.  
+
 #initialize an empty list to store a history of where points have been scored
-score_history = []
+score_pos_history = []
+
+#Keep track of actual score
+score_counter = 0
 
 def update():
     """Checks to see if score should be updated, and adds it to history if so"""
     
-    #TODO: Check whether or not roomba is currently in range of a place it
-    #previously scored.  Below code should not run if within range.  
+    if not is_near_already_scored():    
+        #If theres a force field, add current position to score history
+        if ir_sensors.dock_force_field_detected == True:
+            score_pos_history += [(position.x, position.y)]
 
-    #If theres a force field, add current position to score history
-    if ir_sensors.dock_force_field_detected == True:
-        score_history += [(position.x, position.y)]
+        #Then update score counter
+        score_counter += 1
+
+def is_near_already_scored():
+    """Checks if roomba is near any place in the history where it has already scored.
+    Returns True/False"""
+
+    for x, y in score_pos_history:
+        if x - 1000 < position.x < x + 1000 and y - 1000 < position.y < y + 1000:
+            return True
+    return False
+
+def clear():
+    """Resets score_counter to 0"""
+    score_counter = 0
+
