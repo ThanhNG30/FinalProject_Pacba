9520cc2cd30794740023a42bb07eaaffcf80074f
Ryan Fox <ryan.fox75@qc.quincycollege.edu>
2026-05-07 13:30:38 -0400

    Changed IR_Sensors implementation to be a component class of Pacba

--- ir_senors.py
+++ ir_senors.py
@@ -2,15 +2,10 @@
 Author: Ryan Fox
 PACba Final Project
 Module: ir_sensors.py
-Description: Provides booleans containing whether or not a virtual wall
-or dock force field has been detected with the ir sensors.  Contains a function
-to update those booleans, and functions to return them 
+Description: Provides and IR_Sensors class, designed to be a sub-component
+of the Pacba class.  That class should initialize this one
+when created.  
 """
-
-# Constants for holding current detection status, initialized to false
-# when module is first loaded (the first time it is imported in the process)
-virtual_wall_detected = False
-dock_force_field_detected = False
 
 # Query command and packets
 QUERY_LIST = [149]
@@ -26,44 +21,48 @@ BYTES_RETURNED = 4 #one for each infrared packet
 # indicate one or both buoy's are present as well, but we don't care about those. 
 FORCE_FIELD_CHARS = [242, 250, 246, 254, 161, 165 ,169, 173]
 
-def update(ser):
-    """
-    Updates the virtual_wall_detected and dock_force_field_detected booleans.
-    Takes one argument, a pySerial object, which it uses to communicate with the
-    Roomba.  
-    """
-    # Get sensor data.
-    ser.write(bytearray(QUERY_LIST + [len(REQUIRED_PACKETS)] + REQUIRED_PACKETS))
-    sensors = ser.read(BYTES_RETURNED)
+class IR_Sensors:
+    """Class for handling IR sensor data for a Pacba"""
 
-    # Tell python we want to use the module level variables, otherwise it will
-    # create local variables with the same name when we assign them.  
-    global virtual_wall_detected
-    global dock_force_field_detected
+    def __init__(self, pacba):
+        """Inits an IR_Sensors object, takes a  parent pacba object"""
+        self.pacba = pacba
+        # Constants for holding current detection status, initialized to false
+        # when module is first loaded (the first time it is imported in the process)
+        self.virtual_wall_detected = False
+        self.dock_force_field_detected = False
 
-    # We want to set these back to False in case they were True last time update
-    # was called, we will then check if they should be set to True again.  
-    virtual_wall_detected = False
-    dock_force_field_detected = False
+    def update(self):
+        """
+        Updates the virtual_wall_detected and dock_force_field_detected booleans.
+        """
+        # Get sensor data.
+        self.pacba.ser.write(bytearray(QUERY_LIST + [len(REQUIRED_PACKETS)] + REQUIRED_PACKETS))
+        sensors = self.pacba.ser.read(BYTES_RETURNED)
 
-    # Check for virtual wall
-    if sensors[3] == 1:
-        virtual_wall_detected = True
+        # We want to set these back to False in case they were True last time update
+        # was called, we will then check if they should be set to True again.  
+        self.virtual_wall_detected = False
+        self.dock_force_field_detected = False
 
-    # Check for force field
-    for sensor in sensors[0:3]:
-        for char in FORCE_FIELD_CHARS:
-            if sensor == char:
-                dock_force_field_detected = True
+        # Check for virtual wall
+        if sensors[3] == 1:
+            self.virtual_wall_detected = True
 
-def get_virtual_wall_detected():
-    """
-    Returns True if a virtual wall was detected, False otherwise
-    """
-    return virtual_wall_detected
+        # Check for force field
+        for sensor in sensors[0:3]:
+            for char in FORCE_FIELD_CHARS:
+                if sensor == char:
+                    self.dock_force_field_detected = True
 
-def get_dock_force_field_detected():
-    """
-    Returns True if a dock force field was detected, False otherwise
-    """
-    return dock_force_field_detected
+    def virtual_wall_detected(self):
+        """
+        Returns True if a virtual wall was detected, False otherwise
+        """
+        return self.virtual_wall_detected
+
+    def dock_force_field_detected(self):
+        """
+        Returns True if a dock force field was detected, False otherwise
+        """
+        return self.dock_force_field_detected
