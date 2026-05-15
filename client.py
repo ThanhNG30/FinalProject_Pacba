e9f9de7f20f29e4f8b35eb50b96b07265cbe08a3
Thanh Nguyen <jessicanguyen3005@gmail.com>
2026-05-15 10:29:00 -0400

    clean up run()

--- client.py
+++ client.py
@@ -1,24 +1,22 @@
 from socket import *
 
-client = socket(AF_INET, SOCK_STREAM)
+server = socket(AF_INET, SOCK_STREAM)
 host = "192.168.50.100"
 port = 5150
 # Function for connection
 def connect():
-    client.connect((host, port))
+    server.connect((host, port))
     print("Conected to server:")
 # Function to run the cleint loop.
 def run(pacba_is_caught):
     msg = ""
     if pacba_is_caught:
         msg = "caught"
-    else:
-        msg = "not caught"
             
     # convert string to bytes
     data = str.encode()
     # send encode message to the server
-    cserver.send(data)
-    if text == "exit":
-         break
-client.close()
+    server.send(data)
+
+def end():
+    server.close()
